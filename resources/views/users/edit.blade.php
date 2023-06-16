b5c3ff5f91b8523258ef4b75eb2aab65142761f2
Katerine Morcillo <katerine.morcillo@misena.edu.co>
2023-06-16 17:08:14 -0500

    Correcciones de módulo Usuarios

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,18 +8,9 @@
             <div class="col-12">
                 <div class="card-body">
                     <div class="card border-warning border mb-3 mt-5">
-                        <div class="row ">
-                              <!--Título Editar proveedor-->
-                            <div class="col-2">
-                                <div class="page-title-box">
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <h4>Editar Usuarios</h4>
-                                        </ol>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-------------------->
+                        <div class="card-header">
+                            <h1 class="ms-4">Editar usuario</h1> 
+                            <h1 class="ms-4">{{ $user->names }}</h1>
                         </div>
                         <div class="card-body">
                             <form action="{{route('usuarios.actualizar', $user->id)}}" method="post">
@@ -27,6 +18,17 @@
                                 @csrf
                                 <table class="table table-centered table-nowrap table-hover mb-0">
                                     <thead>
+                                        <tr>
+                                            <th>Compañia</th>
+                                            <td>
+                                                <select name="company_id" class="form-select">
+                                                    <option value="">Seleccionar...</option>
+                                                    @foreach($companies as $company)
+                                                        <option value="{{ $company->id }}" @if($user->company_id == $company->id) selected @endif>{{ $company->name }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </td>
+                                        </tr>
                                         <tr>
                                             <th>Nombre </th>
                                             <td><input type="text" class="form-control" name="names" value="{{ $user->names }}" required></td>
@@ -36,6 +38,27 @@
                                             <td><input type="text" class="form-control" name="surnames" value="{{ $user->surnames }}" required></td>
                                         </tr>
                                         <tr>
+                                            <th>Rol</th>
+                                            <td>
+                                                <select name="rol_id" class="form-select">
+                                                    <option value="">Seleccionar...</option>
+                                                    @foreach($roles as $rol)
+                                                        <option value="{{ $rol->id }}" @if($user->rol_id == $rol->id) selected @endif>{{ $rol->name }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Tipo de documento</th>
+                                            <td>
+                                                <select name="document_id" class="form-select">
+                                                    <option value="">Seleccionar...</option>
+                                                    @foreach($documents as $document)
+                                                        <option value="{{ $document->id }}" @if($user->document_id == $document->id) selected @endif>{{ $document->acronym }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </td>
+                                        </tr>
                                             <th>Numero Documento</th>
                                             <td><input type="number" class="form-control" name="document_number" value="{{ $user->document_number }}" required></td>
                                         </tr>
@@ -50,10 +73,20 @@
                                         <tr>
                                             <th>Correo electrónico</th>
                                             <td><input type="email" class="form-control" name="email" value="{{ $user->email }}" required></td>
-                                        </tr>   
+                                        </tr>  
+                                        <tr>
+                                            <th>Contraseña</th>
+                                            <td><input type="password" class="form-control"name="password" value="{{ $user->password }}"></td>
+                                        </tr> 
                                         <tr>
                                             <th>genero</th>
-                                            <td><input type="texto" class="form-control" name="gender" value="{{ $user->gender_id }}" required></td>
+                                            <td>
+                                                <select name="gender" class="form-control">
+                                                    <option value="gender">Seleccionar...</option>
+                                                    <option value="masculino">Masculino</option>
+                                                    <option value="femenino">Femenino</option>
+                                                </select>
+                                            </td>
                                         </tr>                         
                                         <br>
                                         <br>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,17 +2,30 @@
 @section('content')
 
 
-<div class="content">
+<div class="content mt-4">
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
                 <div class="card border-success border mb-3">
-                    <div class="card-header"></div>
-                    <h1 class="ms-3">Detalles del proveedor</h1>
+                    <div class="card-header">
+                        <h1 class="ms-3">Detalles del usuario</h1> 
+                        <h1 class="ms-3">{{ $user->names }}</h1>
+                    </div>
                     <div class="card-body">
                         <table class="table table-centered table-nowrap table-hover mb-0">
         
                             <!--Inicio de Tabla Detalles-->
+                            <tr>
+                                <th>Compañia</th>
+                                <td>
+                                    @foreach($companies as $company)
+                                        @if($user->company_id == $company->id)
+                                            {{ $company->name }}
+                                        @endif
+                                    @endforeach
+
+                                </td>
+                            </tr>
                             <tr>
                                 <th>Nombre</th>
                                 <td>{{ $user->names }}</td>
@@ -21,6 +34,27 @@
                                 <th>Apellido</th>
                                 <td>{{ $user->surnames }}</td>
                             </tr>
+                            <tr>
+                                <th>Rol</th>
+                                <td>
+                                    @foreach($roles as $rol)
+                                        @if($user->rol_id == $rol->id)
+                                            {{ $rol->name }}
+                                        @endif
+                                    @endforeach
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Tipo de documento</th>
+                                <td>
+                                    @foreach($documents as $document)
+                                        @if ($user->document_id == $document->id)
+                                            {{ $document->name }}
+                                        @endif
+                                    @endforeach
+                                </td>
+                            </tr>
+                          
                             <tr>
                                 <th>Número Documento</th>
                                 <td>{{ $user->document_number }}</td>
@@ -38,8 +72,12 @@
                                 <td>{{ $user->email}}</td>
                             </tr>
                             <tr>
-                                <th>Estado:</th>
-                                <td>{{ $user-> status  }}</td>
+                                <th>Estado</th>
+                                <td>{{ $user-> status }}</td>
+                            </tr>
+                            <tr>
+                                <th>Género</th>
+                                <td>{{ $user-> gender }}</td>
                             </tr>
                         </table>
                         <br>
